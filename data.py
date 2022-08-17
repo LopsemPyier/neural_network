aa9f1059e839c53f9a390548cfe4ae781ece02cb
LopsemPyier <LopsemPyier@gmail.com>
2022-08-17 18:22:50 +0200

    Added dataset loaders

---
+++ data.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def load_images(filename):
+    with open(filename, "rb") as f:
+        magic_number = int.from_bytes(f.read(4), "big")
+        nb_images = int.from_bytes(f.read(4), "big")
+        rows = int.from_bytes(f.read(4), "big")
+        columns = int.from_bytes(f.read(4), "big")
+
+        buffer = f.read(nb_images * rows * columns)
+        data = np.frombuffer(buffer, dtype=np.uint8).astype(np.float32)
+        data = data.reshape(nb_images, rows*columns, 1)
+        data /= 255
+        return data
+
+
+def load_labels(filename):
+    with open(filename, "rb") as f:
+        magic_number = int.from_bytes(f.read(4), "big")
+        nb_labels = int.from_bytes(f.read(4), "big")
+
+        buffer = f.read(nb_labels)
+        data = np.frombuffer(buffer, dtype=np.uint8).astype(np.int64)
+        labels = [np.array([0.0 for _ in range(10)]) for _ in range(nb_labels)]
+        for i, label in enumerate(data):
+            labels[i][label] = 1.0
+        return labels
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from aiaiai import AI
 import constant
 import numpy as np
+import data
 
 
 def main():
+    images = data.load_images("dataset/train-images-idx3-ubyte")
+    labels = data.load_labels("dataset/train-labels-idx1-ubyte")
+    print(labels[2])
+
     ai = AI(constant.layers, constant.f)
     print(ai.run(np.random.rand(784)))
 
