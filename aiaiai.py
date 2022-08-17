fe767b715eff7061bd849c5e30d4069da120d2e8
LopsemPyier <LopsemPyier@gmail.com>
2022-08-17 19:31:40 +0200

    Cleaned aiaiai.py

--- aiaiai.py
+++ aiaiai.py
@@ -1,22 +1,20 @@
 import constant
 import numpy as np
 
+
 class AI:
     def __init__(self, layers, f):
-        self.f=f
+        self.f = f
         self.weights = [np.random.rand(layers[i], layers[i-1]) for i in range(1, len(layers))]
-        self.bias =[np.random.rand(layers[i]) for i in range(1, len(layers))]
-        
-        
-        
+        self.bias = [np.random.rand(layers[i]) for i in range(1, len(layers))]
 
-    def train(self):
+    def train(self, inputs, targets):
         pass
 
     def run(self, inputs):
-        vect=np.array(inputs)
+        vect = np.array(inputs)
         for i in range(len(self.weights)):
-            vect=self.f(self.weights[i] @ vect + self.bias[i])
+            vect = self.f(self.weights[i] @ vect + self.bias[i])
         return vect 
 
 
